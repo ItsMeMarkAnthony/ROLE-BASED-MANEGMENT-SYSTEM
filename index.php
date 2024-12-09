7401c5b437cfd58fdc7ccc0ebe968e9e6479e30e
ItsMeMarkAnthony <salazarmarkanthonyremegio@gmail.com>
2024-12-10 03:44:53 +0800

    updated index,SCREENSHOTS

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ $result = $conn->query($sql);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Holy Infant Academy</title>
+    <title>ROLE-BASED MANAGEMENT</title>
     <style>
         /* Global Styles */
         body {
